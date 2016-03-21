a695092fe955625c0ee5f3d40caabc3c0e44682f
benholtzmansmith <benjamin.holtzman.smith@gmail.com>
2016-03-21 16:06:55 -0400

    using root host

--- src/main/scala/BitcoinMixer/BitcoinMixer.scala
+++ src/main/scala/BitcoinMixer/BitcoinMixer.scala
@@ -39,11 +39,13 @@ object BitcoinMixer {
 
     val myPort = Properties.envOrElse("PORT", "8080").toInt
 
+    println(myPort)
+
     println("Starting server!")
 
     scheduleEvery(10.seconds)(())
 
-    IO(SprayHttp) ? SprayHttp.Bind(service, interface = "localhost", port = myPort)
+    IO(SprayHttp) ? SprayHttp.Bind(service, interface = "0.0.0.0", port = myPort)
   }
 }
 
